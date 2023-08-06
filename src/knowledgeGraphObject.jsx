6d07b94a2503bd81575f87cb9fd7a712baa04a5c
Sebastian Heiden <sebastian.heiden1@mailbox.tu-dresden.de>
2023-08-06 14:49:47 +0000

    Change cursor type to "grab"

--- src/knowledgeGraphObject.jsx
+++ src/knowledgeGraphObject.jsx
@@ -3,5 +3,5 @@ import Badge from 'react-bootstrap/Badge';
 
 export function KnowledgeGraphObject(props) {
     const item = props.item;
-    return <ListGroup.Item variant={props.variant} key={item.objectId}><Badge bg="primary">{item.type}</Badge> {item.title ?? item.name} {props.children}</ListGroup.Item>
+    return <ListGroup.Item style="cursor: grab;" variant={props.variant} key={item.objectId}><Badge bg="primary">{item.type}</Badge> {item.title ?? item.name} {props.children}</ListGroup.Item>
 }
